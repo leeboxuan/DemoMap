fb30e8147b6d04c7a8a4d6cae1aae964ab2cf0f2
16046473 <leeboxuan@hotmail.com>
2018-07-09 11:48:35 +0800

    Initial commit

---
+++ app/src/main/java/com/weebly/leeboxuan/demomap/MainActivity.java
@@ -0,0 +1,33 @@
+package com.weebly.leeboxuan.demomap;
+
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button bt1, btn2, btn3;
+    private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        SupportMapFragment mapFragment = (SupportMapFragment) fm.findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+            } 
+        });
+    }
+}
